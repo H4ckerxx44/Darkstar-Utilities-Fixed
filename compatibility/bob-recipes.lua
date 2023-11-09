66593bb599fa73a0a07bb713327bbe87100794e4
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2023-11-09 08:45:25 +0100

    Warnings and Changelog.

--- compatibility/bob-recipes.lua
+++ compatibility/bob-recipes.lua
@@ -1,13 +1,13 @@
 if mods["bobplates"] then
-  data:extend({
-    {
-      type = "recipe",
-      name = "lithium-plate-bob-conversion",
-      category = "smelting",
-      enabled = true,
-      energy_required = 7.5,
-      ingredients = { { "lithium", 20 } }, -- So re-naming all "lithium-plate" to "lithium" like the recipe then it will auto convert to bob's version every time the 2 mods loads togheter and also improved compatibility with both Bob, Angels.
-      result = "lithium-plate"
-    },
-  })
+		data:extend({
+		{
+			type = "recipe",
+			name = "lithium-plate-bob-conversion",
+			category = "smelting",
+			enabled = true,
+			energy_required = 7.5,
+			ingredients = { { "lithium", 20 } }, -- So re-naming all "lithium-plate" to "lithium" like the recipe then it will auto convert to bob's version every time the 2 mods loads togheter and also improved compatibility with both Bob, Angels.
+			result = "lithium-plate"
+		},
+	})
 end
