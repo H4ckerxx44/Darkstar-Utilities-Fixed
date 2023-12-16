0a476e92db3265c973d276d00f20fb1a81cc2feb
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2023-12-16 14:24:30 +0100

    File fixes

--- prototype/autoplace-controls.lua
+++ prototype/autoplace-controls.lua
@@ -1,25 +1,23 @@
-data:extend(
-        {
-            {
-                type = "autoplace-control",
-                name = "gold-ore",
-                richness = true,
-                order = "d-a",
-                category = "resource"
-            },
-            {
-                type = "autoplace-control",
-                name = "dark-lithium-ore",
-                richness = true,
-                order = "d-a",
-                category = "resource"
-            },
-            {
-                type = "autoplace-control",
-                name = "lead-ore",
-                richness = true,
-                order = "d-b",
-                category = "resource"
-            }
-        }
-)
+data:extend({
+    {
+        type = "autoplace-control",
+        name = "gold-ore",
+        richness = true,
+        order = "d-a",
+        category = "resource"
+    },
+    {
+        type = "autoplace-control",
+        name = "dark-lithium-ore",
+        richness = true,
+        order = "d-a",
+        category = "resource"
+    },
+    {
+        type = "autoplace-control",
+        name = "lead-ore",
+        richness = true,
+        order = "d-b",
+        category = "resource"
+    }
+})
